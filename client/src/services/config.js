a17a8bc57fcc1a7cadf74826edb780fba182de44
Minh Phung <nmphung97@gmail.com>
2023-06-29 14:36:36 +0700

    update baseURL

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { localService } from './localService';
 
-const BASE_URL = 'http://localhost:8080/api/';
+const BASE_URL = 'https://vintube-server.onrender.com/api';
 
 const configHeader = () => {
   return {
